82348535d902771f0e0cba0d8c3f741ef1994ad8
Sarah Schultz <sarah@productplan.com>
2016-11-10 09:02:18 -0700

    Moar kittehs

--- bin/more-images.sh
+++ bin/more-images.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # @see https://github.com/Giphy/GiphyAPI#search-endpoint
-raw=$(curl -s "http://api.giphy.com/v1/gifs/search?q=kitten&limit=101&api_key=dc6zaTOxFJmzC")
+raw=$(curl -s "http://api.giphy.com/v1/gifs/search?q=cute-kitten&limit=101&api_key=dc6zaTOxFJmzC")
 
 # Adjust as necessary.
-for i in `seq 1 100`;
+for i in `seq 201 300`;
 do
   url=$(echo $raw | ./jq-osx-amd64 ".data[$i].images.fixed_height.mp4" | sed 's/"//g')
   name=$(printf "%03d" $i)
