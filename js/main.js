ecf7e6a583e7712854eb336b6313176fe4f492e4
Carl Wiedemann <carl.wiedemann@gmail.com>
2016-11-09 20:37:32 -0700

    image preloading

--- js/main.js
+++ js/main.js
@@ -4,21 +4,32 @@
 
   var MAX = 100;
 
+  var frame = document.getElementById('frame');
+
+  // A function that populates the frame.
+  var populateFrame = function(img_tag) {
+    console.log(img_tag);
+    while (frame.firstChild) frame.removeChild(frame.firstChild);
+    frame.appendChild(img_tag);
+  };
+
   // A function that shows kittens.
   var showKitten = function() {
     var rand = Math.floor(Math.random() * MAX) + 1;
 
     var img = new Image();
     var name = ('000' + rand).substr(-3, 3);
+
     img.src = '/images/' + name + '.gif'
 
     var img_tag = document.createElement('img');
     img_tag.setAttribute('src', img.src);
 
-    // @todo Preloading.
-    var frame = document.getElementById('frame');
-    while (frame.firstChild) frame.removeChild(frame.firstChild);
-    frame.appendChild(img_tag);
+    // Populate frame post-preload.
+    img.onload = function () {
+      populateFrame(img_tag);
+    };
+
   };
 
   // Bind to our friend the button.
