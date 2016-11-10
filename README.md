66a16e44fb64d6143127af9ec6ff69e53e72d830
Carl Wiedemann <carl.wiedemann@gmail.com>
2016-11-10 13:16:23 -0700

    s/webm/webp/

--- README.md
+++ README.md
@@ -8,18 +8,6 @@ Some emergencies call for kittens
 
 ---
 
-## Building urself
-
-0. Get a mac
-1. Install homebrew
-2. Install ffmpeg
-
-    brew install ffmpeg --with-libvpx --with-libvorbis --with-fdk-aacc
-
-3. Edit/run `more-images.sh` to get the mp4s
-4. Run `convert-to-webm.sh` to get the webm
-
 ## Acknowledgements
 
 * Uses https://github.com/stedolan/jq to parse images nicely from Giphy 🏆
-* ffmpeg
--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 
   var MAX = 300;
 
+  var USE_WEBP = false;
+
+  var modernizr_ready = false;
+
   var frame = document.getElementById('frame');
 
   // A function that shows kittens.
@@ -12,34 +16,49 @@
 
     var name = ('000' + rand).substr(-3, 3);
 
-    var source_tag_mp4 = document.createElement('source');
-    source_tag_mp4.setAttribute('src', '/images/' + name + '.mp4');
-    source_tag_mp4.setAttribute('type', 'video/mp4');
+    var kitten_imagery;
 
-    var source_tag_webm = document.createElement('source');
-    source_tag_webm.setAttribute('src', '/images/' + name + '.webm');
-    source_tag_webm.setAttribute('type', 'video/webm');
+    if (USE_WEBP) {
+      var image_tag = document.createElement('img');
+      image_tag.setAttribute('src', '/images/' + name + '.webp');
 
-    var video_tag = document.createElement('video');
-    video_tag.setAttribute('autoplay', 'autoplay');
-    video_tag.setAttribute('autostart', 'autostart');
-    video_tag.setAttribute('loop', 'loop');
+      kitten_imagery = image_tag;
+    }
+    else {
+      var source_tag_mp4 = document.createElement('source');
+      source_tag_mp4.setAttribute('src', '/images/' + name + '.mp4');
+      source_tag_mp4.setAttribute('type', 'video/mp4');
 
-    video_tag.appendChild(source_tag_mp4);
-    video_tag.appendChild(source_tag_webm);
+      var video_tag = document.createElement('video');
+      video_tag.setAttribute('autoplay', 'autoplay');
+      video_tag.setAttribute('autostart', 'autostart');
+      video_tag.setAttribute('loop', 'loop');
+
+      video_tag.appendChild(source_tag_mp4);
+
+      kitten_imagery = video_tag;
+    }
+
 
     while (frame.firstChild) frame.removeChild(frame.firstChild);
-    frame.appendChild(video_tag);
+    frame.appendChild(kitten_imagery);
 
   };
 
   // Bind to our friend the button.
   document.getElementById('funk').addEventListener('click', function(){
-    showKitten();
+    if (modernizr_ready) {
+      showKitten();
+    }
     return false;
   });
 
-  // Initial.
-  showKitten();
+  Modernizr.on('webpanimation', function(supported) {
+    modernizr_ready = true;
+    USE_WEBP = !!supported;
+    console.log('USE_WEBP');
+    console.log(USE_WEBP);
+    showKitten();
+  });
 
 })(window, document);
